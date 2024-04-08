bfc1eb4dd512dba0c34e7b15d22f92daf45594bb
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2024-04-08 20:05:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const xClass = 'x';
-const circleClass = 'circle';
-const winningCombinations = [
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -9,74 +9,81 @@ const winningCombinations = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-];
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
-const cells = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-const winningMessage = document.getElementById('winning-message');
-const restartButton = document.getElementById('restart-button');
-const winningMessageText = document.querySelector('[data-winning-message-text]');
-let circleTurn = false;
+startGame()
 
-startGame();
-
-restartButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', startGame)
 
 function startGame() {
-  circleTurn = false;
-  cells.forEach(cell => {
-    cell.classList.remove(xClass, circleClass);
-    cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  winningMessage.classList.remove('show');
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
 }
 
-function handleClick(event) {
-  const cell = event.target;
-  const currentClass = circleTurn ? circleClass : xClass;
-  placeMark(cell, currentClass);
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
   if (checkWin(currentClass)) {
-    endGame(false);
+    endGame(false)
   } else if (isDraw()) {
-    endGame(true);
+    endGame(true)
   } else {
-    swapTurns();
-    setBoardHoverClass();
+    swapTurns()
+    setBoardHoverClass()
   }
 }
 
 function endGame(draw) {
   if (draw) {
-    winningMessageText.innerText = 'Draw!';
+    winningMessageTextElement.innerText = 'Draw!'
   } else {
-    winningMessageText.innerText = `${circleTurn ? 'O\'s' : 'X\'s'} Win!`;
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
   }
-  winningMessage.classList.add('show');
+  winningMessageElement.classList.add('show')
 }
 
 function isDraw() {
-  return cells.every(cell => cell.classList.contains(xClass) || cell.classList.contains(circleClass));
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
 }
 
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass);
+  cell.classList.add(currentClass)
 }
 
 function swapTurns() {
-  circleTurn = !circleTurn;
+  circleTurn = !circleTurn
 }
 
 function setBoardHoverClass() {
-  board.classList.remove(xClass, circleClass);
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
   if (circleTurn) {
-    board.classList.add(circleClass);
+    board.classList.add(CIRCLE_CLASS)
   } else {
-    board.classList.add(xClass);
+    board.classList.add(X_CLASS)
   }
 }
 
 function checkWin(currentClass) {
-  return winningCombinations.some(combination => combination.every(index => cellElements[index].classList.contains(currentClass)));
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
 }
